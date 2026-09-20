07bce754bc8be5baa361c21b24cd968930ef79f5
Max-Buhler <maxbuhler50@gmail.com>
2026-09-20 10:45:19 +0200

    implemented naive id query
    Co-authored-by: nicolaiandersson <nicolaiaagaard1@gmail.com>

--- src/id_query_naive.c
+++ src/id_query_naive.c
@@ -1,37 +1,38 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-#include <stdint.h>
-#include <errno.h>
-#include <assert.h>
 
-#include "record.h"
 #include "id_query.h"
+#include "record.h"
 
 struct naive_data {
   struct record *rs;
   int n;
 };
 
-struct naive_data* mk_naive(struct record* rs, int n) {
-  // TODO
-  assert(0);
+struct naive_data *mk_naive(struct record *rs, int n) {
+  struct naive_data *nd = malloc(sizeof(struct naive_data));
+  (*nd).rs = rs;
+  (*nd).n = n;
+  return nd;
 }
 
-void free_naive(struct naive_data* data) {
-  // TODO
-  assert(0);
+void free_naive(struct naive_data *data) { free(data); }
+
+const struct record *lookup_naive(struct naive_data *data, int64_t needle) {
+  for (int i = 0; i < (*data).n; i++) {
+    if ((*data).rs[i].osm_id == needle) {
+      return &(*data).rs[i];
+    }
+  }
+  return NULL;
 }
 
-const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
-  // TODO
-  assert(0);
-}
-
-int main(int argc, char** argv) {
-  return id_query_loop(argc, argv,
-                    (mk_index_fn)mk_naive,
-                    (free_index_fn)free_naive,
-                    (lookup_fn)lookup_naive);
+int main(int argc, char **argv) {
+  return id_query_loop(argc, argv, (mk_index_fn)mk_naive,
+                       (free_index_fn)free_naive, (lookup_fn)lookup_naive);
 }
